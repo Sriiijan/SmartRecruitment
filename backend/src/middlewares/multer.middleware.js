7661c9f51e9850f84ebb97d5db70e8679fc1467d
Srijan Sasmal <srijansasmal88@gmail.com>
2026-05-24 21:30:53 +0530

    Refactor multer middleware to enhance filename generation with unique timestamps; improve code readability and structure

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,14 +1,28 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) { // cb: call back
-        cb(null, "./public/temp") // destination folder
+
+    destination: function (req, file, cb) {
+
+        cb(null, "./public/temp");
     },
+
     filename: function (req, file, cb) {
-        cb(null, file.originalname)
+
+        const uniqueName =
+            Date.now() +
+            "-" +
+            file.originalname.replace(/\s+/g, "");
+
+        cb(null, uniqueName);
     }
-})
-  
+});
+
 export const upload = multer({
-    storage
-})
+
+    storage,
+
+    limits: {
+        fileSize: 10 * 1024 * 1024, // 10MB
+    }
+});
